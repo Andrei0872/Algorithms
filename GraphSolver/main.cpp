e0ff8a691a0005093f59bbe3d6fc1f10a0a14e51
Andrei Gatej <andreigtj01@gmail.com>
2021-10-15 19:30:09 +0300

    cpp(GraphSolver): add `solveMinEdgesRequiredFromSource`

--- GraphSolver/main.cpp
+++ GraphSolver/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <queue>
 
 using namespace std;
 
@@ -19,6 +20,7 @@ class Graph {
     void setUndirected(bool);
 
     int getNrOfConnectedComponents();
+    int* showMinEdgesRequiredFromSource(int);
 };
 
 Graph::Graph(int n, int m) : nrNodes(n), nrEdges(m) {
@@ -73,6 +75,40 @@ void Graph::DFS (int nodeIdx, int componentRootIdx, bool& hadUnvisitedNodes) {
   }
 }
 
+int* Graph::showMinEdgesRequiredFromSource (int sourceNodeIdx) {
+  int *pathCosts = new int[nrNodes + 1];
+  queue<int> q;
+
+  for (int i = 1; i <= nrNodes; i++) {
+    pathCosts[i] = -1;
+  }
+
+  pathCosts[sourceNodeIdx] = 0;
+  
+  int crtNodeIdx;
+  q.push(sourceNodeIdx);
+  while (!q.empty()) {
+    crtNodeIdx = q.front();
+    q.pop();
+
+    visited[crtNodeIdx] = true;
+
+    for (auto childNodeIdx : adjList[crtNodeIdx]) {
+      if (visited[childNodeIdx]) {
+        continue;
+      }
+
+      q.push(childNodeIdx);
+      pathCosts[childNodeIdx] = pathCosts[childNodeIdx] == -1 ? pathCosts[crtNodeIdx] + 1 : min(pathCosts[childNodeIdx], pathCosts[crtNodeIdx] + 1);
+    }
+  }
+
+  // for (int i = 1; i <= nrNodes; i++) {
+  //   cout << pathCosts[i] << '\n';
+  // }
+  return pathCosts;
+}
+
 // Problem: https://infoarena.ro/problema/dfs
 // Tests: https://infoarena.ro/job_detail/2784008
 void solveNrOfConnectedComponents () {
@@ -95,8 +131,36 @@ void solveNrOfConnectedComponents () {
   out.close();
 }
 
+// Problem: https://infoarena.ro/problema/bfs
+// Tests: https://infoarena.ro/job_detail/2784104
+void solveMinEdgesRequiredFromSource () {
+  int N, M, S;
+
+  ifstream in("bfs.in");
+  in >> N >> M >> S;
+  
+  Graph g(N, M);
+  g.setUndirected(false);
+
+  int x, y;
+  for (int i = 1; i <= M; i++) {
+    in >> x >> y;
+    g.addEdge(x, y);
+  }
+
+  ofstream out("bfs.out");
+  int* pathCosts = g.showMinEdgesRequiredFromSource(S);
+  for (int i = 1; i <= N; i++) {
+    out << pathCosts[i] << ' ';
+  }
+
+  in.close();
+  out.close();
+}
+
 int main () {
   solveNrOfConnectedComponents();
+  // solveMinEdgesRequiredFromSource();
 
   return 0;
 }
